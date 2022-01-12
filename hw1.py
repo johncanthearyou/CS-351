717a585d12964a5c4507f5b530c8047e924efe3f
John Stockton <johnstocktoniv@gmail.com>
2022-01-11 20:46:21 -0500

    init

---
+++ hw1.py
@@ -0,0 +1,73 @@
+#CS351 -- HW1
+#John Stockton
+#Winter 2022
+
+# Precondition: len(nums1) == len(nums2) and nums1 != []
+# Input: (list of number, list of number) -> number
+# Output: Greatest absolute difference between numbers at corresponding
+#         positions in nums1 and nums2.
+def greatest_difference(nums1, nums2):
+    maxDiff = 0;
+    currDiff = maxDiff;
+    for idx in range(len(nums1)):
+        currDiff = abs( nums1[idx]-nums2[idx] );
+        maxDiff = maxDiff if (currDiff<=maxDiff) else currDiff;
+    return maxDiff;
+
+
+#Input: (list of int, int) -> bool
+#Output: True if and only if it is possible to form amount, which is a 
+#        number of cents, using exactly two coins, which can be of any 
+#        of the denominations in denoms.
+def can_pay_with_two_coins(denoms, amount):
+    i=0;
+    while( i<len(denoms) ):
+        j=i;
+        while( j<len(denoms) ):
+            if( denoms[i]+denoms[j] == amount ): return True;
+            j += 1;
+        i += 1;
+    #If we get here, we tried all combos and nothing worked, return false
+    return False;
+    
+# print( can_pay_with_two_coins([1, 5, 10, 25], 35) ) #True
+# print( can_pay_with_two_coins([1, 5, 10, 25], 20) ) #True
+# print( can_pay_with_two_coins([1, 5, 10, 25], 12) ) #False
+
+
+#Input: (str) -> bool
+#Output: True if every letter in s is fluffy. Fluffy letters are those that
+#        appear in the word 'fluffy'.
+def all_fluffy(s):
+    fluffyLetters = "fluy"
+    for idx in range( len(s) ):
+        if( fluffyLetters.find(s[idx])==-1 ): return False;
+    return True;
+
+# print( all_fluffy('fullfly') ) #True
+# print( all_fluffy('firefly') ) #False
+
+
+#TODO:
+#Precondition: s.isdigit() holds for each string s in nums_list.
+#Input: (list of str) -> int
+#Output: the sum of all the digits in all strings in nums_list.
+def digital_sum(nums_list):
+    total = 0;
+    for i in range( len(nums_list) ):
+        for j in range( len(nums_list[i]) ):
+            total += int( nums_list[i][j] ); #Read char value as int
+    return total;
+
+print( digital_sum(['64', '128', '256']) ) #34
+print( digital_sum(['12', '3']) ) #6
+
+
+#TODO:
+#Input: (int) -> int
+#Output: the number of steps it takes to reach 1, by applying the two steps
+#        of the Collatz conjecture beginning from n.
+def count_collatz_steps(n):
+    return -1;
+
+# print( count_collatz_steps(6) )#8
