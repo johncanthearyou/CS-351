a1a3c4cd6e05df1147994c110a1d0db06d125b06
John Stockton <johnstocktoniv@gmail.com>
2022-01-31 18:16:53 -0500

    HW2 Test

--- HW2/client.py
+++ HW2/client.py
@@ -1,1 +1,20 @@
- 
+ #!/usr/bin/python3           # This is client.py file
+
+import socket
+
+# create a socket object
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+
+# get local machine name
+host = "ip-172-31-28-157"                           
+
+port = 9999
+
+# connection to hostname on the port.
+s.connect((host, port))                               
+
+# Receive no more than 1024 bytes
+msg = s.recv(1024)                                     
+
+s.close()
+print (msg.decode('ascii'))
