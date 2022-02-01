6350387cafa6a1a2a3ae49947cbe6be5be599cca
John Stockton <johnstocktoniv@gmail.com>
2022-01-31 20:27:45 -0500

    HW2 V0.2

--- HW2/server.py
+++ HW2/server.py
@@ -1,40 +1,42 @@
 import socket
 from subprocess import check_output
 
+
 def get_wc(file_name):
     return check_output(["wc", "-l", file_name])
 
+
 # create a socket object
 server_socket = socket.socket(
-	        socket.AF_INET, socket.SOCK_STREAM) 
+    socket.AF_INET, socket.SOCK_STREAM)
 
 # get local machine name
-host = socket.gethostname()                           
-port = 9999                                           
+host = socket.gethostname()
+port = 9999
 
 # bind to the port
 server_socket.bind((host, port))
-print("Ready to serve at> %s:%s" % (host, port) )                               
+print("Ready to serve at> %s:%s" % (host, port))
 
 # queue up to 5 requests
-server_socket.listen(5)                                           
+server_socket.listen(5)
 
 while True:
     # establish a connection
-    clientsocket,addr = server_socket.accept()      
+    client_socket, addr = server_socket.accept()
 
     print("Got a connection from %s" % str(addr))
 
-    #Get File Name & Data
-    file_name = server_socket.recv(1024)
-    file_data = server_socket.recv(1024)
+    # Get File Name & Data
+    file_name = client_socket.recv(1024)
+    file_data = client_socket.recv(1024)
 
-    #Open file and write received data to it
+    # Open file and write received data to it
     file = open(file_name, "w")
     file.write(file_data)
     file.close
 
-    msg = get_wc( file_name )
-    clientsocket.send(msg.encode())
+    msg = get_wc(file_name)
+    client_socket.send(msg.encode())
 
-    clientsocket.close()
+    client_socket.close()
