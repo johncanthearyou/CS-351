bd2ebc166ff9099e97e18f5b3256339ac1410e12
John Stockton <johnstocktoniv@gmail.com>
2022-02-04 13:01:55 -0500

    HW2 V1.0

--- HW2/client.py
+++ HW2/client.py
@@ -2,28 +2,36 @@ import socket
 from sys import argv
 from time import sleep
 
-# create a socket object
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# This function will create a socket and send a file name and its associated data
+#   to a server, which will compute the number of lines, words, and characters in
+#   that file. The server will return a formatted string containing the number of lines, 
+#   words, and characters in that file. The client then prints that returned
+#   string to the standard ouput (terminal)
+def main():
+    # create a socket object
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Hardcode Server IP address
-host = 'JWS-MacBook-Pro.local'
-port = 4455
+    # Hardcode Server IP address
+    host = ''
+    port = 4455
 
-# connection to hostname on the port.
-client_socket.connect((host, port))
+    # connection to hostname on the port.
+    client_socket.connect((host, port))
 
-# Open & Read File
-file_name = argv[1]
-file = open(file_name, 'r')
-file_data = file.read()
+    # Open & Read File
+    file_name = argv[1]
+    file = open(file_name, 'r')
+    file_data = file.read()
 
-# Send File Name & Data from server
-client_socket.send(file_name.encode('ascii'))
-sleep(1)
-client_socket.send(file_data.encode('ascii'))
+    # Send File Data to the server
+    client_socket.send(file_data.encode('ascii'))
 
-message = client_socket.recv(4096).decode('ascii')
-print(message)
+    message = client_socket.recv(4096).decode('ascii')
+    print(f'File: {file_name}\n{message}')
 
-file.close()
-client_socket.close()
+    file.close()
+    client_socket.close()
+
+# Invoke main
+if(__name__ == '__main__'):
+    main()
--- HW2/server.py
+++ HW2/server.py
@@ -1,6 +1,13 @@
 import socket
 import os
 
+file_name = 'tmpfile'
+
+# Function get_wc takes a file object and computes the number of lines,
+#   words, and characters within that file
+# Inputs: File, the file object to be processed for the counts
+# Outputs: str, a formatted message giving the number of lines,
+#             words, and characters of a given file
 def get_wc(file):
     lines = 0
     words = 0
@@ -12,44 +19,52 @@ def get_wc(file):
         words += len(line.split())
         chars += len(line)
 
-    return f'File: {file_name}\n\tLines: {lines}\n\tWords: {words} \n\tCharacters: {chars}'
+    return f'\tLines: {lines}\n\tWords: {words} \n\tCharacters: {chars}'
 
+# This function creates a socket and listens for incoming requests until the program is terminated
+# Upon Client Connection:
+#   1. A client will connect and send this server a file name and its content
+#   2. his server will count the number of lines, words, and characters
+#   3. This server will send a formatted string to the client of the
+#      number of lines, words, and characters and close the connection to the client
+def main():
+    # create a socket object
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# create a socket object
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # get local machine name
+    host = socket.gethostname()
+    port = 4455
 
-# get local machine name
-host = socket.gethostname()
-port = 4455
+    # Bind server to the IP:Port address
+    server_socket.bind((host, port))
+    server_socket.listen()
+    print(f'Ready to serve at> {host}:{port}')
 
-# Bind server to the IP:Port address
-server_socket.bind((host, port))
-server_socket.listen()
-print(f'Ready to serve at> {host}:{port}')
+    while True:
+        # Accept incoming connection
+        client_socket, addr = server_socket.accept()
+        print(f'Got a connection from {addr}')
 
-while True:
-    # Accept incoming connection
-    client_socket, addr = server_socket.accept()
-    print(f'Got a connection from {addr}')
+        # Get file data
+        file_data = client_socket.recv(4096).decode()
+        print('\tReceived File Data\n')
 
-    # Get File Name & Data
-    file_name = client_socket.recv(4096).decode()
-    print(f'\tGot File Name: {file_name}')
-    file_data = client_socket.recv(4096).decode()
-    print('\tReceived File Data\n')
+        # Create file and write received data to it
+        file = open(file_name, 'w+')
+        file.write(file_data)
+        file.seek(0)
 
-    # Create file and write received data to it
-    file = open(file_name, 'w+')
-    file.write(file_data)
-    file.seek(0)
+        # Process file data, send result to client
+        msg = get_wc(file).encode('ascii')
+        client_socket.send(msg)
 
-    # Process file data, send result to client
-    msg = get_wc(file).encode('ascii')
-    client_socket.send(msg)
+        # Delete file from the directory
+        file.close()
+        os.remove(file_name)
 
-    # Don't keep received file around
-    file.close()
-    os.remove(file_name)
+        # Close connection to this client
+        client_socket.close()
 
-    # Close connection to this client
-    client_socket.close()
+# Invoke main
+if(__name__ == '__main__'):
+    main()
