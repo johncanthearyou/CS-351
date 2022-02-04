cb6e94c5c80771576bcbe1e3494c033b4bc1e0c5
John Stockton <johnstocktoniv@gmail.com>
2022-02-04 08:20:49 -0500

    HW2 V0.0

--- HW2/client.py
+++ HW2/client.py
@@ -1,23 +1,29 @@
 import socket
 from sys import argv
+from time import sleep
 
 # create a socket object
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Hardcode Server IP address
-host = "ip-172-31-28-157"
-port = 9999
+host = 'JWS-MacBook-Pro.local'
+port = 4455
 
 # connection to hostname on the port.
 client_socket.connect((host, port))
 
 # Open & Read File
 file_name = argv[1]
-file = open(file_name, "r")
+file = open(file_name, 'r')
 file_data = file.read()
 
 # Send File Name & Data from server
-client_socket.send(file_name.encode())
-client_socket.send(file_data.encode())
+client_socket.send(file_name.encode('ascii'))
+sleep(1)
+client_socket.send(file_data.encode('ascii'))
 
+message = client_socket.recv(4096).decode('ascii')
+print(message)
+
+file.close()
 client_socket.close()
--- HW2/server.py
+++ HW2/server.py
@@ -1,42 +1,51 @@
 import socket
-from subprocess import check_output
 
+def get_wc(file_contents):
+    lines = 0
+    words = 0
+    chars = 0
+    first_iter = True
+    for char in file_contents:
+        if(char == '\n' or char == ' ' or char == '\t'):
+            if(char == '\n'):
+                lines += 1
+            words += 1
+        chars += 1
 
-def get_wc(file_name):
-    return check_output(["wc", "-l", file_name])
+    return f'File: {file_name}\n\tLines:{lines}\n\tWords:{words} \n\tCharacters:{chars}'
 
 
 # create a socket object
-server_socket = socket.socket(
-    socket.AF_INET, socket.SOCK_STREAM)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # get local machine name
 host = socket.gethostname()
-port = 9999
+port = 4455
 
-# bind to the port
+# Bind server to the IP:Port address
 server_socket.bind((host, port))
-print("Ready to serve at> %s:%s" % (host, port))
-
-# queue up to 5 requests
-server_socket.listen(5)
+server_socket.listen()
+print('Ready to serve at> %s:%s' % (host, port))
 
 while True:
-    # establish a connection
+    # Accept incoming connection
     client_socket, addr = server_socket.accept()
-
-    print("Got a connection from %s" % str(addr))
+    print('Got a connection from %s' % str(addr))
 
     # Get File Name & Data
-    file_name = client_socket.recv(1024).decode()
-    file_data = client_socket.recv(1024).decode()
+    file_name = client_socket.recv(4096).decode()
+    print('\tGot File Name: %s' % file_name)
+    file_data = client_socket.recv(4096).decode()
+    print('\tReceived File Data\n')
 
     # Open file and write received data to it
-    file = open(file_name, "w")
+    file = open(file_name, 'w')
     file.write(file_data)
     file.close
 
-    msg = get_wc(file_name)
-    client_socket.send(msg.encode())
+    # Process file data, send result to client
+    msg = get_wc(file_data).encode('ascii')
+    client_socket.send(msg)
 
+    # Close connection to this client
     client_socket.close()
