dd43c1a50d8735ba2ce64217cab60587b2f917f1
John Stockton <johnstocktoniv@gmail.com>
2022-02-05 17:01:54 -0500

    HW2 V2.0

--- HW2/server.py
+++ HW2/server.py
@@ -55,7 +55,6 @@ def main():
 
         # Get file data
         file_data = client_socket.recv(size).decode()
-        print(file.seek(0, os.SEEK_END))
         print('\tReceived File Data\n')
 
         # Create temporary file and write received data to it
